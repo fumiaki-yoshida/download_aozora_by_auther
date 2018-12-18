5f89c3c42d1f82a1ca5e9ae35c4471c7003278d5
fumiaki-yoshida <fumiakiyoshida1988@gmail.com>
2018-12-19 01:44:14 +0900

    Initial commit

---
+++ aozora_download.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+import requests
+import sys
+import time
+from datetime import datetime
+
+
+df = pd.read_csv('list_person_all_extended.csv', encoding='cp932')
+family_name = input('input auther 姓:')
+first_name = input('input auther 名:')
+df = df[df['姓'] == family_name]
+df = df[df['名'] == first_name]
+if len(df)==0:
+    print('don`t exist name')
+    sys.exit()
+
+url_list = df['テキストファイルURL']
+
+now = datetime.now()
+get_time = now.strftime('%Y%m%d%H%M')
+dir_name = df['姓ローマ字'][:1].to_string() + get_time
+os.mkdir(dir_name)
+os.chdir(dir_name)
+
+for url in url_list:
+    time.sleep(1)
+    file_name = url.split("/")[-1]
+    r = requests.get(url)
+    if r.status_code == 200:
+        f = open(file_name, 'wb')
+        f.write(r.content)
+        f.close()
